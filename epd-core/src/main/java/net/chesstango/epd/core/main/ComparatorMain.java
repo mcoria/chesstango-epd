2d355e675f22b94e4f1b9c4c921b4ee7a50f4429
Mauricio Coria <coriamauricio@gmail.com>
2026-09-18 08:27:38 -0300

    Refactor `EpdRebaseModel` to integrate SAN encoding for move representation and update session directories in `ComparatorMain`.

--- epd-core/src/main/java/net/chesstango/epd/core/main/ComparatorMain.java
+++ epd-core/src/main/java/net/chesstango/epd/core/main/ComparatorMain.java
@@ -25,10 +25,10 @@ public class ComparatorMain {
     public static void main(String[] args) {
         printSummaryLegends();
 
-        Path suiteParentDirectory = Path.of("C:\\java\\projects\\chess\\chess-utils\\testing\\EPD\\database");
+        Path suiteParentDirectory = Path.of("C:\\java\\projects\\chess\\chess-utils\\testing\\EPD\\database_workspace");
 
-        new ComparatorMain(suiteParentDirectory, "depth-5-2026-09-15-00-05-v1.10.0")
-                .addSession("depth-5-2026-09-17-09-11-v1.11.0-SNAPSHOT")
+        new ComparatorMain(suiteParentDirectory, "depth-1-2026-09-17-21-45-v1.11.0-SNAPSHOT")
+                .addSession("depth-1-2026-09-17-21-48-v1.11.0-SNAPSHOT")
                 //
                 //
                 .execute();
--- epd-core/src/main/java/net/chesstango/epd/core/report/EpdRebaseModel.java
+++ epd-core/src/main/java/net/chesstango/epd/core/report/EpdRebaseModel.java
@@ -1,8 +1,11 @@
 package net.chesstango.epd.core.report;
 
 import lombok.Getter;
+import net.chesstango.board.moves.Move;
+import net.chesstango.board.moves.MovePromotion;
 import net.chesstango.epd.core.search.EpdSearchResult;
 import net.chesstango.gardel.epd.EPD;
+import net.chesstango.gardel.move.SANEncoder;
 import net.chesstango.reports.Model;
 import net.chesstango.search.SearchResult;
 
@@ -27,6 +30,8 @@ public class EpdRebaseModel implements Model<List<EpdSearchResult>> {
 
 
     EPD convert(EpdSearchResult epdSearchResult) {
+        SANEncoder sanEncoder = new SANEncoder();
+
         EPD epd = epdSearchResult.getEpd();
         SearchResult searchResult = epdSearchResult.getSearchResult();
 
@@ -39,9 +44,29 @@ public class EpdRebaseModel implements Model<List<EpdSearchResult>> {
         newEpd.setHalfMoveClock(epd.getHalfMoveClock());
         newEpd.setFullMoveClock(epd.getFullMoveClock());
 
-        newEpd.setSuppliedMoveStr(searchResult.getBestMove().coordinateEncoding());
+        Move bestMove = searchResult.getBestMove();
+        String currentMoveStr = sanEncoder.encodeAlgebraicNotation(toMove(bestMove), epd.toFEN());
+
+        newEpd.setSuppliedMoveStr(currentMoveStr);
         newEpd.setCentiPawnEvaluation(searchResult.getBestEvaluation().toString());
 
         return newEpd;
+    }
+
+    private net.chesstango.gardel.move.Move toMove(Move playedMove) {
+        net.chesstango.gardel.move.Move.Square from = net.chesstango.gardel.move.Move.Square.of(playedMove.getFrom().square().getFile(), playedMove.getFrom().square().getRank());
+        net.chesstango.gardel.move.Move.Square to = net.chesstango.gardel.move.Move.Square.of(playedMove.getTo().square().getFile(), playedMove.getTo().square().getRank());
+
+        if (playedMove instanceof MovePromotion movePromotion) {
+            return net.chesstango.gardel.move.Move.of(from, to, switch (movePromotion.getPromotion()) {
+                case KNIGHT_WHITE, KNIGHT_BLACK -> net.chesstango.gardel.move.Move.PromotionPiece.KNIGHT;
+                case BISHOP_WHITE, BISHOP_BLACK -> net.chesstango.gardel.move.Move.PromotionPiece.BISHOP;
+                case ROOK_WHITE, ROOK_BLACK -> net.chesstango.gardel.move.Move.PromotionPiece.ROOK;
+                case QUEEN_WHITE, QUEEN_BLACK -> net.chesstango.gardel.move.Move.PromotionPiece.QUEEN;
+                default -> throw new RuntimeException("Invalid promotion " + movePromotion);
+            });
+        } else {
+            return net.chesstango.gardel.move.Move.of(from, to);
+        }
     }
 }
